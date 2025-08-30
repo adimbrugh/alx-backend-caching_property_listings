da80f7f8a2421c073a4a6746389c8470d0adfaa1
adimbrugh <haisamismail71@gmail.com>
2025-08-30 17:54:40 +0400

    Invalidate the all_properties Redis cache on Property create/update/delete using Django signals.

---
+++ properties/__init__.py
@@ -0,0 +1,5 @@
+
+
+default_app_config = "properties.apps.PropertiesConfig"
+# This ensures that the PropertiesConfig class is used to configure the app
+# and that the signals are properly registered when the app is ready.
